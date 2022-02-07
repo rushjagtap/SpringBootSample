9dfc6d048bf874eedf87d1040f3041a0220af92f
rushikesh <rushikeshjagtap0@gmail.com>
2022-02-07 12:39:17 +0000

    initial commit

---
+++ src/main/java/com/ait/spring/pkg/RestHandler.java
@@ -0,0 +1,20 @@
+package com.ait.spring.pkg;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestHandler {
+
+	@RequestMapping("/")
+	public String index()
+	{
+		return "welcome ....";
+	}
+	
+	@RequestMapping("/message")
+	public String index(String message)
+	{
+		return message;
+	}
+}
